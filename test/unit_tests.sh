29aee555c2460a2af74923fdc582201827f22b0d
corti <corti@5d055bd7-163f-4d8d-bd86-7d408c05c84d>
2012-11-16 09:38:48 +0000

    Simplified the sourcing of the tests

--- test/unit_tests.sh
+++ test/unit_tests.sh
@@ -28,10 +28,6 @@ NAGIOS_CRITICAL=1
 NAGIOS_WARNING=2
 NAGIOS_UNKNOWN=3
 
-# configure a trap on exit so that the unit
-# test is executed then the sourced script exits
-trap 'run_tests' EXIT
-
 testDependencies() {
     check_required_prog openssl
     assertNotNull 'openssl not found' "${PROG}"
@@ -48,26 +44,9 @@ testUsage() {
     assertEquals "wrong exit code" ${NAGIOS_UNKNOWN} "$?"
 }    
 
-run_tests() {
+# source the script.
+. ${SCRIPT}  --source-only
 
-    # restore standard output
-    exec 1>&3
-
-    # restore standard error
-    exec 2>&4
-
-    # run shUnit: it will execute all the tests in this file
-    # (e.g., functions beginning with 'test'
-    . ${SHUNIT2}
-
-}
-
-# clone standard output
-exec 3>&1
-# clone standard error
-exec 4>&2
-
-# source the script. The output can be safely redirected to /dev/null
-# as we have saved both file descriptors. They will be restored in the
-# trap handler
-. ${SCRIPT} 1>/dev/null 2>/dev/null
+# run shUnit: it will execute all the tests in this file
+# (e.g., functions beginning with 'test'
+. ${SHUNIT2}
