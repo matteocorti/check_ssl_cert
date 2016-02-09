2b70f5137e540d62397ae02decd9a47ca78aee04
Matteo Corti <matteo@corti.li>
2016-02-09 09:08:31 +0100

    Upodated the check script after the shell-ci-build fix

--- shell-ci-build.sh
+++ shell-ci-build.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
 set -eo pipefail
-
-success() {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] Linting %s...\n" "$1"
-}
-
-fail() {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] Linting %s...\n" "$1"
-  exit 1
-}
-
-check() {
-  local script="$1"
-  shellcheck "$script" || fail "$script"
-  success "$script"
-}
-
+source ./shell-ci-build/build.sh
 check "./check_ssl_cert"
