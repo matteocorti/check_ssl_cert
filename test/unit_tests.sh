e91fcfea70a4778f6a340d414d2195b034d1eabc
matteo@corti.li <matteo@corti.li>
2021-03-10 12:31:18 +0100

    Travis CI - multiple distributions - better test for sieve

--- test/unit_tests.sh
+++ test/unit_tests.sh
@@ -589,13 +589,7 @@ testRequiredProgramPermissions() {
 }
 
 testSieveRSA() {
-    openssl s_client -starttls sieve 2>&1
-
-    echo
-    echo
-    echo --------------------------------------------------------------------------------
-    
-    if ! { openssl s_client -starttls sieve 2>&1 | grep -F -q 'Value must be one of:' || openssl s_client -starttls sieve 2>&1 | grep -F -q 'error: usage:' ; } ; then
+    if ! { openssl s_client -starttls sieve 2>&1 | grep -F -q 'Value must be one of:' || openssl s_client -starttls sieve 2>&1 | grep -F -q 'usage:' ; } ; then
         ${SCRIPT} -P sieve -p 4190 -H mail.aegee.org --rsa
         EXIT_CODE=$?
         assertEquals "wrong exit code" "${NAGIOS_OK}" "${EXIT_CODE}"
@@ -605,7 +599,7 @@ testSieveRSA() {
 }
 
 testSieveECDSA() {
-    if ! { openssl s_client -starttls sieve 2>&1 | grep -F -q 'Value must be one of:' || openssl s_client -starttls sieve 2>&1 | grep -F -q 'error: usage:' ; } ; then
+    if ! { openssl s_client -starttls sieve 2>&1 | grep -F -q 'Value must be one of:' || openssl s_client -starttls sieve 2>&1 | grep -F -q 'usage:' ; } ; then
         ${SCRIPT} -P sieve -p 4190 -H mail.aegee.org --ecdsa
         EXIT_CODE=$?
         assertEquals "wrong exit code" "${NAGIOS_OK}" "${EXIT_CODE}"
