ee177a72e5ea579cc30a93f6d08ae98e08ea3c1b
matteo@corti.li <matteo@corti.li>
2021-03-19 13:36:02 +0100

    Remove spaces at the end of lines

--- test/unit_tests.sh
+++ test/unit_tests.sh
@@ -435,7 +435,7 @@ testBadSSL3DES(){
         assertEquals "wrong exit code" "${NAGIOS_CRITICAL}" "${EXIT_CODE}"
       else
         echo "OpenSSL too old to test 3DES ciphers"
-    fi  
+    fi
 }
 
 testBadSSLNULL(){
@@ -524,7 +524,7 @@ testIPv6() {
 	elif command -v ip > /dev/null && ip addr | grep -q -F inet6 ; then
 	    IPV6=1
 	fi
-	    
+
         if [ -n "${IPV6}" ] ; then
 
 	    echo "IPv6 is configured"
@@ -697,27 +697,27 @@ testCiphersOK() {
     if [ -f /etc/redhat-release ] && grep -q '.*Linux.*release\ 7\.' /etc/redhat-release ; then
         echo 'Skipping tests on CentOS and RedHat 7 since nmap is crashing (core dump)'
     else
-    
+
         # check if nmap is installed
         if command -v nmap > /dev/null ; then
 
             # check if ssl-enum-ciphers is present
             if ! nmap --script ssl-enum-ciphers 2>&1 | grep -q -F 'NSE: failed to initialize the script engine' ; then
-            
+
                 ${SCRIPT} --rootcert-file cabundle.crt -H cloudflare.com --check-ciphers C
                 EXIT_CODE=$?
                 assertEquals "wrong exit code" "${NAGIOS_OK}" "${EXIT_CODE}"
-            
+
             else
                 echo "no ssl-enum-ciphers nmap script found: skipping ciphers test"
             fi
-        
+
         else
             echo "no nmap found: skipping ciphers test"
         fi
 
     fi
-        
+
 }
 
 testCiphersError() {
@@ -729,24 +729,24 @@ testCiphersError() {
 
         # check if nmap is installed
         if command -v nmap > /dev/null ; then
-            
+
             # check if ssl-enum-ciphers is present
             if ! nmap --script ssl-enum-ciphers 2>&1 | grep -q -F 'NSE: failed to initialize the script engine' ; then
-                
+
                 ${SCRIPT} --rootcert-file cabundle.crt -H ethz.ch --check-ciphers A --check-ciphers-warnings
                 EXIT_CODE=$?
                 assertEquals "wrong exit code" "${NAGIOS_CRITICAL}" "${EXIT_CODE}"
-                
+
             else
                 echo "no ssl-enum-ciphers nmap script found: skipping ciphers test"
             fi
-            
+
         else
             echo "no nmap found: skipping ciphers test"
         fi
 
     fi
-    
+
 }
 
 # SSL Labs (last one as it usually takes a lot of time
