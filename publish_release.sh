92406311733399bb960814dedd029c8bcb3e31cf
matteo@corti.li <matteo@corti.li>
2021-04-23 10:54:27 +0200

    Better handling of OpenSSL timeouts

--- publish_release.sh
+++ publish_release.sh
@@ -4,7 +4,7 @@ VERSION=$( head -n 1 VERSION )
 
 echo 'Did you update the RELEASE_NOTES.md? '
 read -r ANSWER
-if [ "x${ANSWER}" = "xy" ] ; then
+if [ "${ANSWER}" = "y" ] ; then
     
     make
     gh release create "v${VERSION}" --title "check_ssl_cert-${VERSION}" --notes-file RELEASE_NOTES.md "check_ssl_cert-${VERSION}.tar.gz" "check_ssl_cert-${VERSION}.tar.bz2"
