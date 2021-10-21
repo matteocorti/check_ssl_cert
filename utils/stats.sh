0838a2969715a741f7d9b9c043b2a2b4404cc557
Matteo Corti <matteo@corti.li>
2021-10-21 19:17:27 +0200

    Fixing Backslash is literal in "\t". Prefer explicit escaping: "\\t".

--- utils/stats.sh
+++ utils/stats.sh
@@ -11,7 +11,7 @@ version=$( head -n 1 VERSION )
 echo "check_ssl_cert version ${version}"
 echo
 
-printf "Authors:\t%s\n" "${authors}"
-printf "Versions:\t%s\n" "${versions}"
+printf "Authors:\\t%s\\n" "${authors}"
+printf "Versions:\\t%s\\n" "${versions}"
 
 echo
