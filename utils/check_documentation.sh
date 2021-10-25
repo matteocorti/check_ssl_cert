a0591496176804113492a2a1d8463dd7980812ae
Matteo Corti <matteo@corti.li>
2021-10-25 18:39:46 +0200

    Added the check on the man page and fixed some errors (not yet all of them)

--- utils/check_documentation.sh
+++ utils/check_documentation.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ERROR=0
+
 # get the help
 HELP=$( ./check_ssl_cert --help )
 
@@ -17,17 +19,27 @@ for option in $( grep '^[ ]*-.*)$' check_ssl_cert | sed -e 's/^[ ]*//' -e 's/)//
 
             if ! echo "${HELP}" | grep -q -- "${option}" ; then
                 echo "Error ${option} is not documented in the help (--help_)"
+                ERROR=1
             fi
 
             # 2) check if the option is documented in README.md
 
             if ! grep -q -- "${option}" README.md ; then
                 echo "Error ${option} is not documented in README.md"
+                ERROR=1
             fi
 
+            # 3) check if the option is documented in the man page
+
+            if ! grep -q -- "${option}" check_ssl_cert.1 ; then
+                echo "Error ${option} is not documented in check_ssl_cert.1"
+                ERROR=1
+            fi
 
             ;;
 
     esac
 
 done
+
+exit "${ERROR}"
