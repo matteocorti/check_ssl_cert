e55cf09ce12ffa121a61a65eae1c875351e0b470
Matteo Corti <matteo@corti.li>
2021-10-27 12:55:56 +0200

    Checking the documention step 1

--- utils/check_documentation.sh
+++ utils/check_documentation.sh
@@ -15,37 +15,63 @@ for option in $( grep '^[ ]*-.*)$' check_ssl_cert | sed -e 's/^[ ]*//' -e 's/)//
             ;;
         *)
 
-            # 0) check if the option is documented in the help.txt file
+            # check if the option is documented in the help.txt file
             if ! grep -q -- "${option}" help.txt ; then
-                echo "Error ${option} is not documented in help.txt"
+                echo "Error: ${option} is not documented in help.txt"
                 ERROR=1
             fi
 
-            # 1) check if the option is documented in check_ssl_cert
+            # check if the option is documented in check_ssl_cert
 
             if ! echo "${HELP}" | grep -q -- "${option}" ; then
-                echo "Error ${option} is not documented in the help (--help_)"
+                echo "Error: ${option} is not documented in the help (--help_)"
                 ERROR=1
             fi
 
-            # 2) check if the option is documented in README.md
+            # check if the option is documented in README.md
 
             if ! grep -q -- "${option}" README.md ; then
-                echo "Error ${option} is not documented in README.md"
+                echo "Error: ${option} is not documented in README.md"
                 ERROR=1
             fi
 
-            # 3) check if the option is documented in the man page
+            # check if the option is documented in the man page
 
             if ! grep -q -- "${option}" check_ssl_cert.1 ; then
-                echo "Error ${option} is not documented in check_ssl_cert.1"
+                echo "Error: ${option} is not documented in check_ssl_cert.1"
                 ERROR=1
             fi
+
 
             ;;
 
     esac
 
 done
+
+# check if the option descriptions are present in all the files
+
+while read line; do
+
+    option=$( echo "${line}" | sed 's/;.*//' )
+    description=$( echo "${line}" | sed 's/[^;]*;//' )
+
+    if ! grep -q "${description}" check_ssl_cert ; then
+        echo "Error: the description of option '${option}' '${description}' is not present in check_ssl_cert"
+        ERROR=1
+    fi
+
+    # if ! grep -q "${line}" check_ssl_cert.1 ; then
+    #     echo "Error: the description of option ${option} '${description}' is not the same in check_ssl_cert.1"
+    #     ERROR=1
+    # fi
+
+    # if ! grep -q "${line}" README.md ; then
+    #     echo "Error: the description of option ${option} '${description}' is not the same in README.md"
+    #     ERROR=1
+    # fi
+
+done < help.txt
+
 
 exit "${ERROR}"
