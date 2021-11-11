435b6b82645365d491b8063f9077131baa6e9d98
Matteo Corti <matteo@corti.li>
2021-11-11 21:36:20 +0100

    Some commit statics

--- utils/stats.sh
+++ utils/stats.sh
@@ -11,15 +11,20 @@ version=$( head -n 1 VERSION )
 echo "check_ssl_cert version ${version}"
 echo
 
-printf "Authors:\\t%s\\n" "${authors}"
-printf "Versions:\\t%s\\n" "${versions}"
+printf "Authors:\\t%10s\\n" "${authors}"
+printf "Versions:\\t%10s\\n" "${versions}"
 
 echo
 
 tests=$( grep -c '^test' test/unit_tests.sh )
 loc=$( grep -c '.' check_ssl_cert )
 
-printf "LoC:\\t\\t%s\\n" "${loc}"
-printf "Tests:\\t\\t%s\\n" "${tests}"
+printf "LoC:\\t\\t%10s\\n" "${loc}"
+printf "Tests:\\t\\t%10s\\n" "${tests}"
 
 echo
+
+commits=$( git log --oneline | wc -l )
+
+printf "Commits:\\t%10s\\n" "${commits}"
+git log --numstat --format="" | awk '{files += 1}{ins += $1}{del += $2} END{printf "File changes\t%10s\nInsertions\t%10s\nDeletions\t%10s\n", files, ins, del}'
