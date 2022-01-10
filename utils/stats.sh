f1d26187d27f851c927ea02e293e23ce23a1b3ac
Matteo Corti <matteo@corti.li>
2022-01-10 12:24:05 +0100

    Some more statistics using the GitHub CLI

--- utils/stats.sh
+++ utils/stats.sh
@@ -18,17 +18,19 @@ echo
 
 printf "Authors:\\t\\t%'10d\\n" "${authors}"
 printf "Versions:\\t\\t%'10d\\n" "${versions}"
+
+releases=$( gh release list -L 1000 | wc -l )
+printf "GH releases:\\t\\t%'10d\\n" "${releases}"
+
 echo
 
 echo "------------------------------------------------------------------------------"
 echo "-- Code"
 echo
 
-tests=$( grep -c '^test' test/unit_tests.sh )
 loc=$( grep -c '.' check_ssl_cert )
 
 printf "LoC:\\t\\t\\t%'10d\\n" "${loc}"
-printf "Tests:\\t\\t\\t%'10d\\n" "${tests}"
 echo
 
 echo "------------------------------------------------------------------------------"
@@ -40,6 +42,22 @@ commits=$( git log --oneline | wc -l )
 printf "Commits:\\t\\t%'10d\\n" "${commits}"
 git log --numstat --format="" | awk '{files += 1}{ins += $1}{del += $2} END{printf "File changes:\t\t%10'"'"'d\nInsertions:\t\t%10'"'"'d\nDeletions:\t\t%10'"'"'d\n", files, ins, del}'
 
+open_issues=$( gh issue list -L 1000 | wc -l )
+closed_issues=$( gh issue list -s closed -L 1000 | wc -l )
+open_bugs=$( gh issue list -L 1000 -l bug | grep -v 'No issue match' | wc -l )
+
+echo
+printf "Open issues:\\t\\t%'10d\\n" "${open_issues}"
+printf "Open bugs:\\t\\t%'10d\\n" "${open_bugs}"
+printf "Closed issues:\\t\\t%'10d\\n" "${closed_issues}"
+
+open_prs=$( gh pr list -L 1000 | wc -l )
+closed_prs=$( gh pr list -s all -L 1000 | wc -l )
+
+echo
+printf "Open pull requests:\\t%'10d\\n" "${open_prs}"
+printf "Total pull requests:\\t%'10d\\n" "${closed_prs}"
+
 echo
 
 echo "------------------------------------------------------------------------------"
@@ -48,5 +66,19 @@ echo
 
 command_line_options=$( sed 's/;.*//' help.txt  | sort  | uniq | wc -l )
 printf "Command line options:\\t%'10d\\n" "${command_line_options}"
+
+echo
+
+echo "------------------------------------------------------------------------------"
+echo "-- Tests"
+echo
+
+tests=$( grep -c '^test' test/unit_tests.sh )
+workflows=$( gh workflow list -L 1000 | wc -l)
+runs=$( gh run list -L 10000 | wc -l )
+
+printf "Tests:\\t\\t\\t%'10d\\n" "${tests}"
+printf "GH workflows:\\t\\t%'10d\\n" "${workflows}"
+printf "GH runs:\\t\\t%'10d\\n" "${runs}"
 
 echo
