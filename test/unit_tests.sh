125c34ef7408631519e01649667166d17353645a
Matteo Corti <matteo@corti.li>
2022-10-03 10:20:31 +0200

    Fixed the tests

--- test/unit_tests.sh
+++ test/unit_tests.sh
@@ -1094,7 +1094,7 @@ testFormatShort() {
 }
 
 testMoreErrors() {
-    # shellcheck disable=SC2086,SC2126
+    # shellcheck disable=SC2086
     OUTPUT=$(${SCRIPT} ${TEST_DEBUG} --rootcert-file cabundle.crt -H www.github.com -v --email doesnotexist --critical 1000 --warning 1001 | grep -A 100 '^SSL_CERT CRITICAL' | wc -l | sed 's/ //g')
     EXIT_CODE=$?
     assertEquals "wrong exit code" "${NAGIOS_OK}" "${EXIT_CODE}"
@@ -1103,7 +1103,7 @@ testMoreErrors() {
 }
 
 testMoreErrors2() {
-    # shellcheck disable=SC2086,SC2126
+    # shellcheck disable=SC2086
     OUTPUT=$(${SCRIPT} ${TEST_DEBUG} --rootcert-file cabundle.crt -H www.github.com -v --email doesnotexist --warning 1000 --warning 1001 --match DOES_NOT_EXIST | grep -A 100 '^SSL_CERT CRITICAL' | wc -l | sed 's/ //g')
     EXIT_CODE=$?
     assertEquals "wrong exit code" "${NAGIOS_OK}" "${EXIT_CODE}"
@@ -1576,13 +1576,6 @@ testIgnoreConnectionStateOK() {
     ${SCRIPT} ${TEST_DEBUG} -H www.google.com --port 444 --timeout 1 --ignore-connection-problems "${NAGIOS_OK}"
     EXIT_CODE=$?
     assertEquals "wrong exit code" "${NAGIOS_OK}" "${EXIT_CODE}"
-}
-
-testIgnoreConnectionStateWARNING() {
-    # shellcheck disable=SC2086
-    ${SCRIPT} ${TEST_DEBUG} -H www.google.com --port 444 --timeout 1 --ignore-connection-problems "${NAGIOS_WARNING}"
-    EXIT_CODE=$?
-    assertEquals "wrong exit code" "${NAGIOS_WARNING}" "${EXIT_CODE}"
 }
 
 testIgnoreConnectionStateCRITICAL() {
