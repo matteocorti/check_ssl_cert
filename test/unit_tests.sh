7ee77a78118e2525f6aab321168e26571107f55e
Matteo Corti <matteo@corti.li>
2023-02-26 22:35:31 +0100

    Fixed the test (videolan.org certificate was not OK)

--- test/unit_tests.sh
+++ test/unit_tests.sh
@@ -996,7 +996,7 @@ testBadSSLSHA12017() {
 
 testRequireOCSP() {
     # shellcheck disable=SC2086
-    ${SCRIPT} ${TEST_DEBUG} --rootcert-file cabundle.crt -H videolan.org --require-ocsp-stapling --ignore-exp
+    ${SCRIPT} ${TEST_DEBUG} --rootcert-file cabundle.crt -H wikipedia.org --require-ocsp-stapling --ignore-exp
     EXIT_CODE=$?
     assertEquals "wrong exit code" "${NAGIOS_OK}" "${EXIT_CODE}"
 }
