56c25d864b41ff0165cc2db4518672bc7c2a7036
Matteo Corti <matteo@corti.li>
2023-10-17 22:13:25 +0200

    Fixed the dir name for Fedora 39

--- utils/prepare_rpm.sh
+++ utils/prepare_rpm.sh
@@ -73,6 +73,12 @@ fc38)
     DIST='fedora'
     RELEASE='38'
     ;;
+fc39)
+    RPMDIR="${WEBROOT}/fedora/39/${ARCH}"
+    SRPMDIR="${WEBROOT}/fedora/39/SRPMS"
+    DIST='fedora'
+    RELEASE='39'
+    ;;
 el7)
     RPMDIR="${WEBROOT}/epel/7/${ARCH}"
     SRPMDIR="${WEBROOT}/epel/7/SRPMS"
